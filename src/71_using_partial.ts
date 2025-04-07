c4adb589d51777f9b4b25051a1c149abe4b76449
rramires <ricardo@flexbr.com>
2025-04-06 22:13:34 -0300

    Added Using Partial.

---
+++ src/71_using_partial.ts
@@ -0,0 +1,18 @@
+// run with: 
+// ts-node ./src/171_using_partial.ts
+
+// In this interface all properties are required
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+// Ok, we can create a user with all properties
+const newUser: User = { id: 1, name: "Tom", email: "tom@email.com"}
+
+// But if we want to update a user, we don't need to provide all properties
+// const updatedUser: User = { id: 1, name: "Tom"} // Error: Property 'email' is missing in type
+
+// But we can use Partial to make all properties optional
+const updatedUser2: Partial<User> = { name: "Tom Hanks"}
